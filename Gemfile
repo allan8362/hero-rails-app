d36166c3847c86b42a4e7dbfda667d48f3f0f3a6
Allan Brown <allanbrown8362@gmail.com>
2020-10-08 15:20:59 +0100

    populate webpages

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+gem 'sassc-rails', '>= 2.1.0'
+gem 'jquery-rails'
+gem 'faker', '~> 2.13'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,31 @@
 class HomeController < ApplicationController
+  before_action :common, :only => [:index, :team]
+  def common
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+    @corp_title = Faker::Job.title 
+    @corp_power = Faker::Company.bs.titleize
+    
+    @rand_num = rand(1..1000)
+    @set_number = rand(1..3)
+    if (@set_number) == 1
+      @set = "set1"
+    elsif (@set_number) == 2
+      @set = "set2"
+    else
+      @set = "set3"
+    end
+
+    @bg = rand(1..2)
+    if @bg == 1
+      @background = "bg1"
+    else
+      @background = "bg2"
+    end
+
+    @avatar = Faker::Avatar.image(slug: @rand_num, size: "250x250", format: "png", set: @set, bgset: @background)
+  end
+
   def index
   end
 
